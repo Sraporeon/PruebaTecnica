a777266fc490567fa631a15f0826f7e8f7212a73
Sraporeon <sanchezanazapata22@gmail.com>
2026-03-04 22:07:08 -0400

    API

---
+++ DGII.Api/DGII.Api/Models/Contribuyente.cs
@@ -0,0 +1,10 @@
+﻿namespace DGII.Api.Models
+{
+    public class Contribuyente
+    {
+        public string RncCedula { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string Tipo { get; set; } = string.Empty;
+        public string Estatus { get; set; } = "activo";
+    }
+}
